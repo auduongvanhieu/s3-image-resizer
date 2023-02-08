c6ead72e367ec55ca0e1fe5f081977f2b529a64e
Nguyễn Văn Hiếu <33143617+auduongvanhieu@users.noreply.github.com>
2023-02-08 09:29:49 +0700

    Init

---
+++ index.js
@@ -0,0 +1,91 @@
+'use strict'
+
+
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3({ signatureVersion: 'v4' });
+const Sharp = require('sharp');
+const PathPattern = /(.*\/)?(.*)\/(.*)/;
+
+// parameters
+const BUCKET = "lifeon-v2"
+const URL = "http://lifeon-v2.s3-website-ap-southeast-1.amazonaws.com"
+const WHITELIST_STR = "1000x1000 1000xAUTO 768xAUTO 576xAUTO 320xAUTO 64xAUTO"
+const WHITELIST = Object.freeze(WHITELIST_STR.split(' '))
+
+exports.handler = async (event) => {
+    const path = event.queryStringParameters.path;
+    const parts = PathPattern.exec(path);
+    const dir = parts[1] || '';
+    const resizeOption = parts[2];  // e.g. "150x150_max"
+    const sizeAndAction = resizeOption.split('_');
+    const filename = parts[3];
+
+    const sizes = sizeAndAction[0].split("x");
+    const action = sizeAndAction.length > 1 ? sizeAndAction[1] : null;
+
+    // Whitelist validation.
+    if (WHITELIST && !WHITELIST.includes(resizeOption)) {
+        return {
+            statusCode: 400,
+            body: `WHITELIST is set but does not contain the size parameter "${resizeOption}"`,
+            headers: { "Content-Type": "text/plain" }
+        };
+    }
+
+    // Action validation.
+    if (action && action !== 'max' && action !== 'min') {
+        return {
+            statusCode: 400,
+            body: `Unknown func parameter "${action}"\n` +
+                'For query ".../150x150_func", "_func" must be either empty, "_min" or "_max"',
+            headers: { "Content-Type": "text/plain" }
+        };
+    }
+
+    try {
+        const data = await S3
+            .getObject({ Bucket: BUCKET, Key: dir + filename })
+            .promise();
+
+        const width = sizes[0] === 'AUTO' ? null : parseInt(sizes[0]);
+        const height = sizes[1] === 'AUTO' ? null : parseInt(sizes[1]);
+        let fit;
+        switch (action) {
+            case 'max':
+                fit = 'inside';
+                break;
+            case 'min':
+                fit = 'outside';
+                break;
+            default:
+                fit = 'cover';
+                break;
+        }
+        const result = await Sharp(data.Body, { failOnError: false })
+            .resize(width, height, { withoutEnlargement: true, fit })
+            .rotate()
+            .toBuffer();
+        console.log("hieunv", "sharp_result", result)
+
+        const putObjectResult = await S3.putObject({
+            Body: result,
+            Bucket: BUCKET,
+            ContentType: data.ContentType,
+            Key: path,
+            CacheControl: 'public, max-age=86400'
+        }).promise();
+        console.log("hieunv", "path", path)
+        console.log("hieunv", "putObjectResult", putObjectResult)
+
+        return {
+            statusCode: 301,
+            headers: { "Location": `${URL}/${path}` }
+        };
+    } catch (e) {
+        return {
+            statusCode: e.statusCode || 400,
+            body: 'Exception: ' + e.message,
+            headers: { "Content-Type": "text/plain" }
+        };
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+{
+  "queryStringParameters": {
+    "path": "design/1000xAUTO/hot_girl_1.jpg"
+  }
+}
+
+http://lifeon-v2.s3-website-ap-southeast-1.amazonaws.com/design/hot_girl_1.jpg
+http://lifeon-v2.s3-website-ap-southeast-1.amazonaws.com/design/1000xAUTO/hot_girl_1.jpg
